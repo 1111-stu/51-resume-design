8430967f5081429f0503ed3a2ed6abb056b4af38
大雄吖 <2981451773@qq.com>
2023-06-03 12:32:21 +0800

    feat: 完成首页页面搭建

---
+++ 51-resume/src/main.ts
@@ -0,0 +1,22 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+import router from './router';
+import '@/style/normalize.css';
+// import component from '@/utils/registerCom';
+
+import ElementPlus from 'element-plus'
+import 'element-plus/dist/index.css'
+// 注册所有图标
+import elementIcons from '@/components/SvgIcon/svgicon';
+import SvgIcon from './components/SvgIcon/SvgIcon.vue';
+
+//图标
+import '@/assets/iconfont/iconfont.js';
+
+const app = createApp(App)
+
+app.use(router);
+app.use(ElementPlus)
+app.use(elementIcons);
+app.component('SvgIcon', SvgIcon);
+app.mount('#app')
---
+++ 51-resume/src/router/index.ts
@@ -0,0 +1,46 @@
+import { createRouter, createWebHashHistory, createWebHistory, RouteRecordRaw } from 'vue-router';
+
+const Design = () => import('@/view/design/index.vue');
+const Index = () => import('@/view/index/index.vue');
+const Custom = () => import('@/view/custom/index.vue');
+
+const routes: Array<RouteRecordRaw> = [
+    {
+        path: '/',
+        name: 'Index',
+        meta: {
+            title: '首页',
+            keepAlive: true,
+            requireLogin: false
+        },
+        component: Index
+    },
+    {
+        path: '/design',
+        name: 'Design',
+        meta: {
+            title: '设计',
+            keepAlive: true,
+            requireLogin: true
+        },
+        component: Design
+    },
+    {
+        path: '/custom',
+        name: 'Custom',
+        meta: {
+            title: '自定义',
+            keepAlive: true,
+            requireLogin: true
+        },
+        component: Custom
+    }
+];
+// const routerHistory = createWebHistory('/');
+const router = createRouter({
+    history: createWebHistory(),
+    routes
+});
+export default router;
+
+
